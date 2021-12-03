17c92ee47e712ee1ee55aa9d2fce136853595e1d
Yohan <yohanlamerand@gmail.com>
2021-12-03 16:14:59 +0100

    redirection to home when logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,14 @@ import { KeycloakService } from 'keycloak-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-keycloak';
 
-  constructor(private keycloakService : KeycloakService){}
+  constructor(private keycloakService: KeycloakService) {}
 
-  logout() : void{
-    this.keycloakService.logout();
+  logout(): void {
+    this.keycloakService.logout('http://localhost:4200');
   }
 }
